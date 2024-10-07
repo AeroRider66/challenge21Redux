5c780c66ab86c7f2045c163657e3c924dbfa5381
AeroRider <dave.butterworth.ee@gmail.com>
2024-10-06 23:47:07 -0500

    again....

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+// import { gql } from '@apollo/client';
+//
+// export const LOGIN_USER = gql`
+//     mutation login($email: String!, $password: String!) {
+//         login(email: $email, password: $password) {
+//             token
+//             #      user {
+//             #        name
+//             #        email
+//             #        games {
+//             #          id
+//             #          title
+//             #          releaseDate
+//             #          platforms
+//             #          genres
+//             #          image
+//             #        }
+//             #        gameCount
+//         }
+//     }
+//     }
+// `;
+//
+// export const ADD_USER = gql`
+//     mutation addUser($name: String!, $email: String!, $password: String!) {
+//         addUser(name: $name, email: $email, password: $password) {
+//             token
+//             #      user {
+//             #        name
+//             #        email
+//             #        games {
+//             #          id
+//             #          title
+//             #          releaseDate
+//             #          platforms
+//             #          genres
+//             #          image
+//             #        }
+//             #        gameCount
+//         }
+//     }
+//     }
+// `;
+//
+// export const SAVE_GAME = gql`
+//     #  mutation saveGame($game: GameInput!) {
+//     #    saveGame(game: $game)
+//     }
+// `;
+//
+// export const REMOVE_GAME = gql`
+//     mutation removeGame($gameId: String!) {
+//         removeGame(gameId: $gameId)
+//     }
+// `;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,14 @@
+// import { gql } from '@apollo/client';
+//
+// export const SAVED_GAMES = gql`
+//     query savedGames {
+//         savedGames {
+//             id
+//             #      title
+//             #      releaseDate
+//             #      platforms
+//             #      genres
+//             #      image
+//             #    }
+//         }
+// `;
